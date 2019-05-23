a855f191830b11526419b387488a3fcfe114e699
nantodevison <nantodevison@gmail.com>
2019-05-23 11:01:18 +0200

    fonction predire trajet ok

--- Carto_Lapi/Analyse/donnees_posttraitees.py
+++ Carto_Lapi/Analyse/donnees_posttraitees.py
@@ -14,6 +14,7 @@ import Connexion_Transfert as ct
 import altair as alt
 import os,math,re, datetime as dt
 from sklearn.cluster import DBSCAN
+from sklearn import svm
 from statistics import mode, StatisticsError
 
 dico_renommage={'created_x':'date_cam_1', 'created_y':'date_cam_2'}
@@ -1145,8 +1146,7 @@ def predire_type_trajet(df_trajet,o_d, date, gamma, C):
     """
     #isoler les données : sur un jour pour une o_d
     test_predict=df_trajet.loc[(df_trajet['o_d']==o_d) &
-                           (df_trajet.set_index('date_cam_1').index.dayofyear==pd.to_datetime(date).dayofyear) &
-                                       (df_trajet['tps_parcours']<pd.Timedelta('4H'))].copy()
+                           (df_trajet.set_index('date_cam_1').index.dayofyear==pd.to_datetime(date).dayofyear)].copy()
     #ajouter des champsde ocnversion des dates en integer, limiter les valeusr sinon pb de mémoire avec sklearn
     test_predict['date_int']=((test_predict.date_cam_1 - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s'))/1000000
     test_predict['temps_int']=(((pd.to_datetime('2018-01-01')+test_predict.tps_parcours) - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s'))/1000000
@@ -1154,20 +1154,24 @@ def predire_type_trajet(df_trajet,o_d, date, gamma, C):
     X=np.array([[a,b] for a,b in zip(test_predict.date_int.tolist(),test_predict.temps_int.tolist())])
     y=np.array(test_predict.filtre_tps.tolist())
     #créer le modele
-    clf = svm.SVC(kernel='rbf', gamma=ga, C=C)
+    clf = svm.SVC(kernel='rbf', gamma=gamma, C=C)
     #alimenter le modele
-    clf.fit(liste_nsample_nfeat, n_sample)
+    clf.fit(X, y)
     #isoler les donner à tester
     df_a_tester=test_predict.loc[(test_predict['filtre_tps']==0) & (test_predict['type']=='85eme_percentile')].copy()
     #liste à tester
     liste_a_tester=np.array([[a,b] for a,b in zip(df_a_tester.date_int.tolist(),df_a_tester.temps_int.tolist())])
-    #dfde résultats de prédiction
+    #df de résultats de prédiction
     df_type_predit=pd.DataFrame([[i, v] for i,v in zip(df_a_tester.index.tolist(),[clf.predict([x])[0] for x in liste_a_tester])], 
                                 columns=['index_source','type_predit'])
-    #mise à jourde la df source
+    #mise à jourde la df source : on met à jour le type
     df_trajet.loc[df_trajet.index.isin(df_type_predit.index_source.tolist()),'type']='predit'
     df_trajet.loc[df_trajet.index.isin(df_type_predit.loc[df_type_predit['type_predit']==1].index_source.tolist()),'filtre_tps']=1
     return df_trajet
+
+def toto():
+    return 1
+
     
     
     
