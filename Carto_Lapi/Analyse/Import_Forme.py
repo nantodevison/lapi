59313760ddd460457e4f834795fa16995269f418
nantodevison <nantodevison@gmail.com>
2019-06-21 15:17:53 +0200

    début mise en forme modules python

---
+++ Carto_Lapi/Analyse/Import_Forme.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 21 juin 2019
+
+@author: martin.schoreisz
+
+Mise en forme des données issues de la Bdd Lapi
+Import des attributs de données nécessaires
+'''
